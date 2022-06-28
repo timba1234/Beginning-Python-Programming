590da5d07cbeaf4d916bc59e27f19c307a481f13
Tim Yamamoto <network911@gmail.com>
2022-06-28 14:55:28 -0600

    Add files via upload

---
+++ End.py
@@ -0,0 +1,41 @@
+# Declare variables
+intA = 50
+intB = 90
+intC = 50
+
+# 1. Create an if-else statement that asks "if intA IS equal to intC"
+# then print the following message if the condition evaluates to True:
+# "intA is equal to intC"
+
+if intA == intC:
+    print("intA is equal to intC")
+
+# 2. Create an if-statement that asks " if intA is NOT equal to intC"
+# then print the following message if the condition evaluates to True:
+# "intA is NOT equakl to intC"
+
+if intA != intC:
+    print("intA is NOT equal to intC")
+
+# 3. Create an if-statement that asks "if intA is greater than or equal to intC"
+# then print the following message if the condition evaluates to True:
+# "intA is greater than or equal to intC"
+
+if intA >= intC:
+    print("intA is greater than or equal to intC")
+
+# 4. Create an if-statement that asks "if intA is less than or equal to intC"
+# then print the following message if the condition evaluates to True:
+# "intA is less than or equal to intC"
+
+if intA <= intC:
+    print("intA is less than or equal to intC")
+
+# 5. Create an if-else statement that asks "if intA IS equal to intC"
+# then print the following message if the condition evaluated to True:
+# "intA is equal to intC" else print "intA is not equal to intC"
+
+if intA == intC:
+    print("intA is equal to intC")
+else:
+    print("intA is not equal to intC")
