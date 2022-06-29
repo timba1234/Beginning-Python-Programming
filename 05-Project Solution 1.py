9be24deb86795ba6cb9ecc2df107211b251f1661
Tim Yamamoto <network911@gmail.com>
2022-06-29 17:04:58 -0600

    Update 05-Project Solution 1.py

--- 05-Project Solution 1.py
+++ 05-Project Solution 1.py
@@ -5,13 +5,13 @@
 # john worked 50 hours this week
 
 
-# declare variables
+# 1. declare variables
 int_hrs_week = 0
 str_employee_name = ""
 
-# Program
+# 2. Program
 str_employee_name = input("Enter employee name: ")
 int_hrs_week = int(input("Enter weekly hours: "))
 
-# Output
+# 3. Output
 print(str_employee_name + " worked " + str(int_hrs_week) + " hours this week")
