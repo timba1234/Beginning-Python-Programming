9f0437c8f98641f90e07099b3fecd1cf6c945e87
Tim Yamamoto <network911@gmail.com>
2022-07-01 07:02:57 -0600

    Create Arithmetic Operators.py

---
+++ Arithmetic Operators.py
@@ -0,0 +1,24 @@
+# Arithmetic Operators
+x = 20
+y = 30
+
+# Addition Operator:
+print(x + y)
+
+# Subtraction Operator:
+print(x - y)
+
+# Multiplication Operator
+print(x * y)
+
+# Division Operator:
+print(x / y)
+
+# Floor Division:
+print(x // 3)
+
+# Modulus Operator:
+print(y % x)
+
+# Exponent Operator:
+print(x ** 3)
