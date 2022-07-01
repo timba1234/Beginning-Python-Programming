3bec44369bdbc3bce19f6487450d443649ff3d3c
Tim Yamamoto <network911@gmail.com>
2022-07-01 07:12:19 -0600

    Create 02-if-and if-else.py

---
+++ 02-if-and if-else.py
@@ -0,0 +1,12 @@
+# Declare variable(s)
+intA = 50
+intB = 90
+intC = 50
+
+# Try an additional comparison operators:
+if intA >= intC:
+    print("intA is greater than OR equal to intC")
+
+
+if intB != intC:
+    print("intA is NOT equal to intC")
