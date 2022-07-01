044e2b809fc67270ca90ba2a5cb764205df36d2a
Tim Yamamoto <network911@gmail.com>
2022-07-01 09:25:41 -0600

    Create Arithmetic_Operator_Presedence.py

---
+++ Arithmetic_Operator_Presedence.py
@@ -0,0 +1,11 @@
+# Declare variables
+
+intA = 3
+intB = 4
+intC = 5
+
+# Python calculates 23 and prints the result:
+print(3 + 4 * 5)
+
+# Overriding Operator presedence Python calculates 35:
+print((3 + 4) * 5)
