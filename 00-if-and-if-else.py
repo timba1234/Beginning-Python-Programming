386cf823a7b02fd4d00e4d730d3ec3ccb48743db
Tim Yamamoto <network911@gmail.com>
2022-07-01 07:11:23 -0600

    Create 00-if-and-if-else.py

---
+++ 00-if-and-if-else.py
@@ -0,0 +1,35 @@
+# Declare variable(s)
+intA = 50
+intB = 90
+
+# ************************************
+# if statements:
+
+# With current values of intA and intB
+# the condition below will return FALSE
+# print statement will NOT RUN
+if intA > intB:
+    print("intA is greater than intB")
+
+
+#  With the current values of intA and intB
+# the conition below will return TRUE
+# print statement will run
+if intA < intB:
+    print("intA is less than intB")
+
+
+# ****************************************
+# if-else conditioanl statement:
+
+if intA < intB:
+    print("intA < intB")
+else:
+    print("intA > intB")
+
+# Take-Away:
+# Using the if-else statement uses a single conditional to evaluate if
+# intA is greater than or less than intB
+# While TWO if-statements are requried for checking if intA is greater than
+# AND less than intB
+#
