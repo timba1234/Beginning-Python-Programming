54fcbb5ef9f7561820f9bfaa5abe1a0ea85049f9
Tim Yamamoto <network911@gmail.com>
2022-07-03 13:09:40 -0600

    Add files via upload

---
+++ if-elif.py
@@ -0,0 +1,10 @@
+intA = 20
+intB = 30
+intC = 50
+
+if intA > intB:
+    print("intA is greater than intB")
+elif intA < intB:
+    print("intA is less than intB")
+else:
+    print("intA must be equal to intB")
